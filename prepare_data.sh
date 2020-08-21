64c86d291c4c33d8c6ba6cbc227d07e9f556b705
abdulrafae <rafae015@gmail.com>
2020-08-21 16:53:35 -0400

    Update prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-#Uncomment to install required packages
-#pip install fairseq
-#pip install sacrebleu
-
-echo 'Cloning Moses github repository (for tokenization scripts)...'
-git clone https://github.com/moses-smt/mosesdecoder.git
-
-echo 'Cloning Subword NMT repository (for BPE pre-processing)...'
-git clone https://github.com/rsennrich/subword-nmt.git
-
 SCRIPTS=mosesdecoder/scripts
 TOKENIZER=$SCRIPTS/tokenizer/tokenizer.perl
 TCROOT=$SCRIPTS/recaser/
