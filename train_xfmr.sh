7a0fbb1c50706dd34aa799caa16b0d94aee1b2be
abdulrafae <rafae015@gmail.com>
2020-09-01 12:00:57 -0400

    Update train_xfmr.sh

--- train_xfmr.sh
+++ train_xfmr.sh
@@ -22,7 +22,7 @@ mkdir -p  $CPKT $LOG
 #(6) Train NMT Model (Transformer)
 CUDA_VISIBLE_DEVICES=$GPU python $FAIRSEQ/train.py $DATA_BIN \
     --arch transformer_iwslt_de_en --share-decoder-input-output-embed \
-    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
+    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.1 \
     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
     --dropout 0.3 --weight-decay 0.0001 \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
