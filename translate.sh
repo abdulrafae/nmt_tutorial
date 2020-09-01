8b2a28adac848eb6c5fc4860d8f160b57cf781b4
abdulrafae <rafae015@gmail.com>
2020-09-01 10:58:17 -0400

    Add files via upload

--- translate.sh
+++ translate.sh
@@ -6,7 +6,7 @@ SRC=fr
 TRG=en
 TEXT=iwslt17.tokenized.fr-en
 
-# Translate and score the test set with sacrebleu
+#(7) Decoding NMT Model
 RESULT=result/iwslt17_${SRC}_${TRG}
 mkdir -p $RESULT
 CUDA_VISIBLE_DEVICES=$GPU fairseq-interactive $DATA_BIN \
@@ -15,11 +15,13 @@ CUDA_VISIBLE_DEVICES=$GPU fairseq-interactive $DATA_BIN \
     --beam 5 --remove-bpe \
     --input $TEXT/test.${SRC} | tee $RESULT/iwslt17.test.${SRC}-${TRG}.${TRG}.sys
 
+#(8) Post-processing	
 # Extract translations from output file 
 grep ^H $RESULT/iwslt17.test.${SRC}-${TRG}.${TRG}.sys | cut -f3- > $RESULT/hypo.tok.sys
 
 # Remove tokenization and truecasing
 cat $RESULT/hypo.tok.sys | $SCRIPTS/recaser/detruecase.perl | $SCRIPTS/tokenizer/detokenizer.perl -l ${TRG} > $RESULT/hypo.sys
 
+#(9) Automatic Evaluation
 # Score the test set with sacrebleu
 cat $RESULT/hypo.sys | sacrebleu --test-set iwslt17 --language-pair ${SRC}-en --tokenize=13a 
